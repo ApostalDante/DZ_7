4bb90085b6f26617cccf3e544cb29b94306bc439
ApostalDante <apostal666666@gmail.com>
2022-10-21 19:43:33 +0300

    create DZ_7

---
+++ core/format.py
@@ -0,0 +1,20 @@
+import convert_format
+import parsing_format
+
+FORMATS = [
+    {
+        'formatDataDelimeter': '\n',
+        'userDataDelimeter': ';',
+        'userPropertiesDelimeter': ',', 
+    },
+    {
+        'formatDataDelimeter': '\t',
+        'userDataDelimeter': ';',
+        'userPropertiesDelimeter': ',',
+    },
+    {
+        'formatDataDelimeter': '%',
+        'userDataDelimeter': ';',
+        'userPropertiesDelimeter': ',',
+    }
+]
